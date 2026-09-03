cc83fd57f92ec316b29c2722ddc49aab6979bfa3
Alan Shahoei <alan.shahoei07@gmail.com>
2026-09-03 22:38:27 +0330

    feat(model): add section model

---
+++ src/App/Models/Section.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use DateTimeImmutable;
+
+readonly class Section
+{
+    public function __construct(
+        private ?int $id = null,
+        private string $name,
+        private string $color = '#3B82F6',
+        private ?DateTimeImmutable $createdAt = null,
+    ) {
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function getCreatedAt(): ?DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+}
