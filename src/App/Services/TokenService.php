9467fa37937407242eb230793b63e1f44e701988
Alan Shahoei <alan.shahoei07@gmail.com>
2026-09-04 18:09:49 +0330

    feat(auth): implement token service

--- src/App/Services/TokenService.php
+++ src/App/Services/TokenService.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 readonly class TokenService
 {
     public function __construct(private string $secret, private int $expiration)
@@ -12,11 +15,15 @@ readonly class TokenService
 
     public function generate(array $payload): string
     {
-        return '';
+        $payload['iat'] = time();
+        $payload['exp'] = time() + $this->expiration;
+
+        return JWT::encode($payload, $this->secret, 'HS256');
     }
 
     public function verify(string $token): array
     {
-        return [];
+        $decoded = JWT::decode($token, new Key($this->secret, 'HS256'));
+        return (array) $decoded;
     }
 }
