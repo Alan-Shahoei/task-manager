432f348da403b1b275b179071f15021ab939cec7
Alan Shahoei <alan.shahoei07@gmail.com>
2026-09-04 08:16:04 +0330

    feat(container): register PDO connection

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use App\Services\ValidatorService;
+use Framework\Connection;
 
 return [
-    ValidatorService::class => fn() => new ValidatorService
+    ValidatorService::class => fn() => new ValidatorService,
+    PDO::class => fn() => Connection::make(),
 ];
