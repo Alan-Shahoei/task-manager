ffbcf230d6984e7a8fc3f4b59891e68349aa3e4b
Alan Shahoei <alan.shahoei07@gmail.com>
2026-09-04 20:59:48 +0330

    feat(framework): add json response handler

---
+++ src/Framework/Response.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+
+class Response
+{
+    public static function json(array $data, int $statusCode = 200): void
+    {
+        http_response_code($statusCode);
+
+        header('Content-Type: application/json');
+
+        echo json_encode($data);
+    }
+}
