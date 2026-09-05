f11474a3d0523c8d5bb7d5498787d26ce3522cb2
Alan Shahoei <alan.shahoei07@gmail.com>
2026-09-05 18:25:17 +0545

    feat(auth): implement current user

--- src/App/Auth/CurrentUser.php
+++ src/App/Auth/CurrentUser.php
@@ -1,18 +1,46 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Auth;
+
+use App\Models\User;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use RuntimeException;
 
 class CurrentUser
 {
-    private ?int $id = null;
+    private ?int $userId = null;
+    private ?User $user = null;
 
-    public function setId(int $id): void
+    public function __construct(private UserRepositoryInterface $userRepository)
     {
-        $this->id = $id;
     }
 
-    public function getId(): ?int
+    public function setId(int $userId): void
     {
-        return $this->id;
+        $this->userId = $userId;
+    }
+
+    public function get(): User
+    {
+        if (!$this->user) {
+            if (!$this->userId) {
+                throw new RuntimeException('Current user is not set');
+            }
+
+            $this->user = $this->userRepository->findById($this->userId);
+
+            if (!$this->user) {
+                throw new RuntimeException('User not found');
+            }
+        }
+
+        return $this->user;
+    }
+
+    public function isAuthenticated(): bool
+    {
+        return $this->userId !== null;
     }
 }
